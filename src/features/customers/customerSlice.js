1d250766eca50f8a97240092dc3fc5154fc5b90e
Cleve-keem <belloharyourmidey@gmail.com>
2025-04-18 12:51:51 -0500

    initial commit

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,31 @@
+// create customer state
+const customerInitialState = {
+  fullName: "",
+  nationalID: null,
+  createdAt: "",
+};
+
+export default function customerReducer(state = customerInitialState, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { combineReducers, createStore } from "redux";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
+
